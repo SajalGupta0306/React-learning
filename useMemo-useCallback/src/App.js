9a52c6af08f28f9e0f92bb6d4c2a77fc72471d9f
I322214 <sajal.gupta@sap.com>
2022-06-17 15:53:35 +0530

    New changes

--- useMemo-useCallback/src/App.js
+++ useMemo-useCallback/src/App.js
@@ -1,12 +1,49 @@
 import "./App.css";
-import Parent from "./components/Parent";
 
-function App() {
+import { useCallback, useEffect, useMemo, useState } from "react";
+import Child from "./Child";
+
+export default function App() {
+  const [userData, setUserData] = useState([]);
+  const [isTrue, setIsTrue] = useState(false);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/comments")
+      .then((res) => res.json())
+      .then((data) => setUserData(data));
+    console.log("inside fetch");
+  }, []);
+
+  const getLongestComment = () => {
+    if (userData.length === 0) {
+      return null;
+    }
+    let longestName = userData[0].name;
+    userData.forEach((val) => {
+      if (val.name.length > longestName.length) {
+        longestName = val.name;
+      }
+    });
+    console.log("inside function");
+    return longestName;
+  };
+
+  const getCallBackLongestComment = useCallback(getLongestComment, [userData]);
+  const memoComment = useMemo(getLongestComment, [userData]);
   return (
     <div className="App">
-      <Parent />
+      <h4>Use Memo Result </h4>
+      {memoComment}
+      <br />
+      <br />
+      <h4>Use Callback Result </h4>
+      <Child callChild={getCallBackLongestComment} />
+      <br />
+      <br />
+      <button onClick={() => setIsTrue(!isTrue)}>Click Me To Toggle</button>
+      <br />
+      <br />
+      {isTrue && <strong>Button Is Clicked</strong>}
     </div>
   );
 }
-
-export default App;
