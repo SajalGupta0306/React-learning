e2412eac07dea37dea6c26632740702195bd623d
Sajal Gupta <gupta.sajal17@gmail.com>
2025-03-18 18:27:23 +0530

    Update thunk.jsx

--- thunk&Saga/thunk.jsx
+++ thunk&Saga/thunk.jsx
@@ -1,6 +1,15 @@
 // Redux Thunk is a middleware that allows you to write action creators that return a function instead of an action object.
 //This function can perform asynchronous operations (e.g., API calls) and dispatch regular Redux actions when the operation is complete.
 
+// Pros:
+// 1. Simple: Easy to set up and use for basic async workflows.
+// 2. Flexible: Allows you to dispatch multiple actions during an async operation.
+// 3. Best For: Small to medium-sized applications with straightforward async logic.
+
+//Cons
+// 1. Callback Hell: For complex logic (e.g., handling multiple async requests), Thunks can become hard to maintain and nested deeply.
+// 2. Limited Control over async operations compared to more advanced solutions.
+
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
